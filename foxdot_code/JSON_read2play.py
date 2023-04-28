957b49c37132a5d0a8beb7157be7d104f81d8b89
中牧泰 <nakamakitai@Tai-Nakamaki-MacBook-Pro-16.local>
2023-04-28 15:53:23 +0900

    read2play の更新版です

--- foxdot_code/JSON_read2play.py
+++ foxdot_code/JSON_read2play.py
@@ -1,6 +1,4 @@
 import json, time, math
-import tkinter as tk
-import tkinter.messagebox as messagebox
 class Track:
     """トラックのクラス"""
     def __init__(self):
@@ -14,7 +12,9 @@ class Track:
         self.duration.extend(new_duration)
         self.suspension.extend(new_suspension)
         self.amplification.extend(new_amplification)
-global_p_uniquenum=0
+global_p_uniquenum=0 # 再生するトラックを管理する。
+# p1とかp2とかの、1とか2の数字。
+# トラックを再生する。
 def trackinstance_to_play(instance:Track,tone:str):
     """トラックインスタンスを用いて、音を出す"""
     global global_p_uniquenum
@@ -22,27 +22,28 @@ def trackinstance_to_play(instance:Track,tone:str):
     +"(track_instance.code,dur=track_instance.duration,"+
     "sus=track_instance.suspension,amp=track_instance.amplification)")
     global_p_uniquenum+=1
+# トラックを停止する
 def trackinstance_to_stop(num_to_stop:int):
     eval("p"+f"{num_to_stop}"+".stop")
 # コード
+# 初期設定
 bpm=120
 Clock.bpm=bpm
-measure_per_loop=4 # 小節数
-beats_per_measure=4
-sampling=1/16*1/bpm # sampling rate
+measure_per_loop=4 # ループあたりの小節数
+beats_per_measure=4 # 小節あたりのビート数
+sampling=1/16*1/bpm # 秒あたりのサンプリング数
 sampling_per_loop=beats_per_measure*measure_per_loop*1/(bpm/60)*1/sampling
-blank=0
-quantize=0 # 0でクオンタイズしない
-checksum=True
+checksum=True # 再生開始時、一発目だけの例外処理用
 for i in range(math.ceil(sampling_per_loop)):
     try:
         with open('./test1.json') as f:
             di = json.load(f)
-        # print(di["time"])
+        # strokeの有無を判定する
+        # 0→1を判定する
         is_stroke=di["is_stroke"]
+        # 音の情報を得る
         guitar_code=di["guitar_code"]
         stroke_duration=di["stroke_duration"]
-        # ストロークがあるなら、その長さを入れる。ないなら0
         stroke_suspension=di["stroke_suspension"]
         stroke_amplification=di["stroke_amplification"]
         if is_stroke|checksum:
@@ -51,18 +52,9 @@ for i in range(math.ceil(sampling_per_loop)):
             stroke_suspension,stroke_amplification)
             if checksum==False: # 前でなってた音を消す
                 stopnum=9 if global_p_uniquenum%9==0 else global_p_uniquenum%9
-                # tk.Tk().withdraw()
-                # messagebox.showinfo('stop', stopnum)
                 trackinstance_to_stop(stopnum)
             trackinstance_to_play(track_instance,"pluck")
-            # tk.Tk().withdraw()
-            # messagebox.showinfo('global', global_p_uniquenum)
             checksum=False
-        # 毎回読み取り
-        # guitar_code=0
-        # ギターコードの種類を指定、実際にはJSONから読む
-        # 数字の形でくる
-        # ギターの有無
-        time.sleep(sampling)    
+        time.sleep(sampling)
     except ValueError:
         pass
